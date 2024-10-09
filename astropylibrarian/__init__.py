47863ca976aa2cd481679188603f55f7303a5a48
Jeff Jennings <jjennings@flatironinstitute.org>
2024-10-09 16:16:51 -0400

    replace deprecated package

--- astropylibrarian/__init__.py
+++ astropylibrarian/__init__.py
@@ -5,10 +5,10 @@ search database.
 
 __all__ = ("__version__",)
 
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+    __version__ = version(__name__)
+except PackageNotFoundError:
     # package is not installed
     __version__ = "0.0.0"
